33e2d79895c5c3a2ef554a5c1c06a5a95acb694e
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2017-09-06 09:54:01 +0200

    get ISS co-ordinates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+interface UserResponse {
+  name: string,
+  latitude: number,
+  longitude: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'International Space Station';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit():void {
+    var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    var lat;
+    var lon;
+    var mapUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+    var mapApiKey = '&key=AIzaSyAi5ihE7UwQGr_qdRmL6_f5FLOeiJAQKWI';
+    var geoCodingUrl = mapUrl;
+
+    this.http.get<UserResponse>(url).subscribe(
+      data => {
+        console.log("My name is" + data.name);
+        console.log("Latitude " + data.latitude);
+        console.log("longitude " + data.longitude);
+        geoCodingUrl += '?latlng=' + data.latitude + ',' + data.longitude + mapApiKey;
+        console.log(geoCodingUrl);
+
+        // geocoding get
+        this.http.get<UserResponse>(geoCodingUrl).subscribe(
+          data => {
+            console.log(data);
+          },
+          (err: HttpErrorResponse) => {
+            if (err.error instanceof Error) {
+              console.log("Client-side error occured");
+            } else {
+              console.log("Server-side error occured");
+            }
+          }
+        )
+
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured");
+        } else {
+          console.log("Server-side error occured");
+        }
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { MyPositionComponent } from './my-position/my-position.component';
@@ -14,7 +16,8 @@ import { MyMissionComponent } from './my-mission/my-mission.component';
     MyMissionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
